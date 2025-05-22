62a788227589ebcd9730b49134aab6bcd8fa2818
maheshwar reddy chappidi <maheshwarreddy.c1729@gmail.com>
2025-05-22 18:51:11 +0530

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -13,7 +13,8 @@ app.include_router(auth_router)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"],  # or list like ["http://localhost:3000"]
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
